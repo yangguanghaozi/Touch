55140ebc907c037ad95c35c5d48f066cc86ae94e
张书澔 <zhangshuhao@le.com>
2017-02-07 17:05:08 +0800

    add shake to lock sreen function

--- app/src/main/java/com/carl/touch/FloatButtonService.java
+++ app/src/main/java/com/carl/touch/FloatButtonService.java
@@ -13,8 +13,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.PixelFormat;
 import android.graphics.Point;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.os.Message;
+import android.os.PowerManager;
 import android.os.Vibrator;
 import android.util.Log;
 import android.view.*;
@@ -22,7 +27,7 @@ import android.view.accessibility.AccessibilityEvent;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-
+import java.util.List;
 
 
 public class FloatButtonService extends AccessibilityService {
@@ -38,8 +43,13 @@ public class FloatButtonService extends AccessibilityService {
     private int clickCount = 0;
     private TouchEventHandler handler = new TouchEventHandler();
 
-    private DevicePolicyManager deviceManger;
-    private ComponentName componentName;
+    private static DevicePolicyManager mDeviceManger;
+    private static ComponentName mComponentName;
+    private static SensorManager mSensorManager;
+    private static Sensor mSensor;
+    private static ShakeListener mShakeListener;
+    private static TurnOverListener mTurnOverListener;
+    private static PowerManager mPowerManager;
 
     private static final int ONE_CLICK = 0x01;
     private static final int TWO_CLICK = 0x02;
@@ -51,19 +61,52 @@ public class FloatButtonService extends AccessibilityService {
     private static final int alpha_delay = 3000;
     private static final int clickDelay = 300;
 
+
+
     @Override
     public void onCreate() {
         super.onCreate();
         createFloatView();
         handler.sendEmptyMessageDelayed(ALPHA_CHANGE, alpha_delay);
         vibrator = (Vibrator) getSystemService(Service.VIBRATOR_SERVICE);
-        deviceManger = (DevicePolicyManager)getSystemService(
+
+        mPowerManager = (PowerManager) this.getSystemService(Context.POWER_SERVICE);
+
+        mDeviceManger = (DevicePolicyManager)getSystemService(
                 Context.DEVICE_POLICY_SERVICE);
-        componentName = new ComponentName(this, LockScreenAdmin.class);
+        mComponentName = new ComponentName(this, LockScreenAdmin.class);
 
-        
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mSensor = null;
+        if (mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) != null){
+            List<Sensor> gravSensors = mSensorManager.getSensorList(Sensor.TYPE_GRAVITY);
+            for(int i=0; i<gravSensors.size(); i++) {
+                if ((gravSensors.get(i).getVendor().contains("Google Inc.")) &&
+                        (gravSensors.get(i).getVersion() == 3)){
+                    // Use the version 3 gravity mSensor.
+                    mSensor = gravSensors.get(i);
+                    Log.d(TAG, "Get gravity mSensor");
+                }
+            }
+        }
+        if (mSensor == null){
+            // Use the accelerometer.
+            if (mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null){
+                mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+                Log.d(TAG, "Get accelerometer mSensor");
+            }
+            else{
+                Log.d(TAG, "Can not get gravity or accelerometer mSensor");
+            }
+        }
+        mShakeListener = new ShakeListener();
+        mSensorManager.registerListener(mShakeListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
+        mTurnOverListener = new TurnOverListener();
+        mSensorManager.registerListener(mTurnOverListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
     }
+
+
 
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
@@ -261,13 +304,22 @@ public class FloatButtonService extends AccessibilityService {
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(1, notificationBuilder.build());
         mWindowManager.removeView(mFloatLayout);
+
+        //When hide, cancel shack listener
+        mSensorManager.unregisterListener(mShakeListener);
     }
 
     private void lock(){
-        boolean active = deviceManger.isAdminActive(componentName);
+
+        if (!mPowerManager.isInteractive()){
+            Log.d(TAG, "Already lock screen");
+            return;
+        }
+
+        boolean active = mDeviceManger.isAdminActive(mComponentName);
         if (active) {
             Log.d(TAG, "lock now");
-            deviceManger.lockNow();
+            mDeviceManger.lockNow();
         }
         else {
             Log.d(TAG, "Start activity");
@@ -279,6 +331,97 @@ public class FloatButtonService extends AccessibilityService {
 
     public static void resume() {
         mWindowManager.addView(mFloatLayout, wmParams);
+        mSensorManager.registerListener(mShakeListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    class ShakeListener implements SensorEventListener{
+
+        private static final int FORCE_THRESHOLD = 350;
+        private static final int TIME_THRESHOLD = 100;
+        private static final int SHAKE_TIMEOUT = 500;
+        private static final int SHAKE_DURATION = 1000;
+        private static final int SHAKE_COUNT = 3;
+
+        private float mLastX=-1.0f, mLastY=-1.0f, mLastZ=-1.0f;
+        private long mLastTime;
+        private int mShakeCount = 0;
+        private long mLastShake;
+        private long mLastForce;
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            long now = System.currentTimeMillis();
+
+            if ((now - mLastForce) > SHAKE_TIMEOUT) {
+                mShakeCount = 0;
+            }
+
+            if ((now - mLastTime) > TIME_THRESHOLD) {
+                long diff = now - mLastTime;
+                float speed = Math.abs(event.values[0] + event.values[1] + event.values[2] - mLastX - mLastY - mLastZ) / diff * 10000;
+                if (speed > FORCE_THRESHOLD) {
+                    if ((++mShakeCount >= SHAKE_COUNT) && (now - mLastShake > SHAKE_DURATION)) {
+                        mLastShake = now;
+                        mShakeCount = 0;
+                    /*if (mShakeListener != null) {
+                        mShakeListener.onShake();
+                    }*/
+                        lock();
+                        Log.d(TAG,"On shake");
+                    }
+                    mLastForce = now;
+                }
+                mLastTime = now;
+                mLastX = event.values[0];
+                mLastY = event.values[1];
+                mLastZ = event.values[2];
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    }
+
+    class TurnOverListener implements SensorEventListener{
+        private static final int TIME_DURATION = 2000;
+        private static final int TIME_THRESHOLD = 100;
+        private static final float COORDINATE_POSITIVE = 9.0f;
+        private static final float COORDINATE_NEGATIVE = -9.0f;
+
+        private float mLastZ = -1.0f;
+        private long lastUpdate;
+        private boolean isUp = false, isDown = false;
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+
+            long now = System.currentTimeMillis();
+            float nowZ = event.values[2];
+//            Log.d(TAG, "coordinate Z=" + nowZ);
+            if ((now - lastUpdate) > TIME_THRESHOLD) {
+                if (isUp == false ) {
+                    if (nowZ > COORDINATE_POSITIVE) {
+                        isUp = true;
+                    }
+                } else {
+                    if (nowZ < COORDINATE_NEGATIVE) {
+                        Log.d(TAG, "Turn over");
+                        lock();
+                        isUp = false;
+                        lastUpdate = now;
+                    } else if ((now - lastUpdate) > TIME_DURATION){
+                        isUp = false;
+                        lastUpdate = now;
+                    }
+                }
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+        }
     }
 
     class TouchEventHandler extends Handler {
@@ -321,9 +464,4 @@ public class FloatButtonService extends AccessibilityService {
             super.handleMessage(msg);
         }
     }
-
-
-
-
-
 }
